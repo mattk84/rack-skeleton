765a0e153abcae2c30a8b225954b147bdf6f93c1
Lee Green <lee.x.green@gmail.com>
2013-04-12 19:16:25 -0400

    empty base route -- sinatra/base

---
+++ routes/base.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+#
+require 'sinatra/base'
+
+module RackSkeleton
+    class BaseRoute < Sinatra::Base
+        # define some common "before" / "configure" settings, etc.
+    end
+end
